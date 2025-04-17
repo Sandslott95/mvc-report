09a4575d98edd777c9ed11f1e74236513d705724
Oscar Sandberg <oscar.c.sandberg@gmail.com>
2025-04-17 14:29:31 +0200

    Lagt upp report texten

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -15,35 +15,35 @@ class ApiController extends AbstractController
     }
 
     #[Route('/api/quote', name: 'quote')]
-public function quote(): Response
-{
-    $quotes = [
-        [
-            'quote' => "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-            'author' => "Winston Churchill"
-        ],
-        [
-            'quote' => "Appear weak when you are strong, and strong when you are weak.",
-            'author' => "Sun Tzu"
-        ],
-        [
-            'quote' => "Alea iacta est. (Tärningen är kastad.)",
-            'author' => "Julius Caesar"
-        ],
-        [
-            'quote' => "Veni, vidi, vici. (I came, I saw, I conquered.)",
-            'author' => "Julius Caesar"
-        ],
-    ];
+    public function quote(): Response
+    {
+        $quotes = [
+            [
+                'quote' => "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+                'author' => "Winston Churchill"
+            ],
+            [
+                'quote' => "Appear weak when you are strong, and strong when you are weak.",
+                'author' => "Sun Tzu"
+            ],
+            [
+                'quote' => "Alea iacta est. (Tärningen är kastad.)",
+                'author' => "Julius Caesar"
+            ],
+            [
+                'quote' => "Veni, vidi, vici. (I came, I saw, I conquered.)",
+                'author' => "Julius Caesar"
+            ],
+        ];
 
-    $selected = $quotes[array_rand($quotes)];
+        $selected = $quotes[array_rand($quotes)];
 
-    return $this->json([
-        'quote' => $selected['quote'],
-        'author' => $selected['author'],
-        'date' => (new \DateTime())->format('Y-m-d'),
-        $timestamp = (new \DateTime('now', new \DateTimeZone('Europe/Stockholm')))->format('Y-m-d H:i:s'),
-    ]);
-}
+        return $this->json([
+            'quote' => $selected['quote'],
+            'author' => $selected['author'],
+            'date' => (new \DateTime())->format('Y-m-d'),
+            $timestamp = (new \DateTime('now', new \DateTimeZone('Europe/Stockholm')))->format('Y-m-d H:i:s'),
+        ]);
+    }
 
 }
